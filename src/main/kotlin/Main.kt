69eba51e364081c9cc3992e64d553512f2faed3f
Gabriel Miranda <qwerlolgabriel@gmail.com>
2025-04-21 14:52:30 -0300

    First commit: include API and init project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+package br.com.alluGames
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+fun main() {
+    val client: HttpClient = HttpClient.newHttpClient()
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .build()
+    // código omitido
+
+    val response = client
+        .send(request, HttpResponse.BodyHandlers.ofString())
+
+    val json = response.body()
+    println(json)
+}
